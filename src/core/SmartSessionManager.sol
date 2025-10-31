6c93462cc0befa59a9438a5c42ce197286e3a224
highskore <luka@racki.dev>
2025-10-31 02:02:05 +0100

    feat: rework storage

--- src/core/SmartSessionManager.sol
+++ src/core/SmartSessionManager.sol
@@ -61,18 +61,24 @@ abstract contract SmartSessionManager is NonceManager, ISmartSessionEmissary {
 
     // -- Permission Storage -- //
 
-    /// @notice Mapping of lockTags to enabled permission IDs per account, these are sessions
-    ///         used for verifyClaim and verifyExecution functions
-    mapping(bytes12 lockTag => EnumerableSet.Bytes32Set permissionIDs) internal $lockTagPermissions;
+    /// @notice Set of enabled permission IDs per smart account
+    EnumerableSet.Bytes32Set internal $enabledSessions;
+
+    // -- Locktag Config Storage -- //
+
+    /// @notice Maps enabled lockTags per account
+    EnumerableSet.Bytes32Set internal $enabledLockTags;
 
     // -- Policy Storage -- //
 
     /// @notice Mapping of erc1271 policies organized by permission IDs and smart account
     Policy internal $erc1271Policies;
-    /// @notice Mapping of action policies organized by action IDs and permission IDs
-    EnumerableActionPolicy internal $actionPolicies;
     /// @notice Set of all enabled ERC7739 configurations for each smart account and permissionId
     EnumerableERC7739Config internal $enabledERC7739;
+    /// @notice Mapping of lockTag to claim policies
+    mapping(bytes12 lockTag => Policy claimPolicies) internal $claimPolicies;
+    /// @notice Mapping of lockTag to enabled action policies
+    mapping(bytes12 lockTag => EnumerableActionPolicy) internal $actionPolicies;
 
     // -- Validator Config Storage -- //
 
@@ -80,11 +86,6 @@ abstract contract SmartSessionManager is NonceManager, ISmartSessionEmissary {
     ///         addresses
     mapping(PermissionId permissionId => mapping(address smartAccount => SignerConf conf)) internal
         $sessionValidators;
-
-    // -- Locktag Config Storage -- //
-
-    /// @notice Maps enabled lockTags per account
-    EnumerableSet.Bytes32Set internal $enabledLockTags;
 
     /*//////////////////////////////////////////////////////////////
                            SESSION MANAGEMENT
